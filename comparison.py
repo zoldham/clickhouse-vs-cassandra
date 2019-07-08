c31339ba22c8bee68bbbdbba59cdb2703ee07093
Zach <zoldham@korewireless.com>
2019-07-08 16:15:52 -0400

    Finished updating the comparision except for artifical load prior to the queries, and finalization of the actual queries that will be tested.

--- comparison.py
+++ comparison.py
@@ -6,13 +6,16 @@ import numpy as np
 import csv 
 from pandas.io.json import json_normalize
 import time
+import random
 
 # Global vars
 num_rows_list = [5000, 10000, 100000, 500000, 1000000]
 num_cols = 42
-num_repetitions = 5
+num_repetitions = 11
 output_file = "output.txt"
+csv_filename = "output.csv"
 file = open(output_file, "w")
+csv_file = open(csv_filename, "w")
 clkhs_instance = '192.168.5.60'
 cass_instance = 'dev-cassandra.ksg.int'
 clkhs_port = 9000
@@ -23,77 +26,81 @@ clkhs_table_definition = ("CREATE TABLE radius.udr ( \n" +
     ") ENGINE = MergeTree() \n" + 
     "PARTITION BY toYYYYMM(CreateDate) \n" + 
     "ORDER BY tuple()")
-
 # TODO: Update this definition if I add indicies
 cass_table_definition = ("CREATE TABLE \"CassandraPractice\".udr_copy1 (\n" +
-"	partitionhash int,\n" +
-"	hashcode text,\n" +
-"	accountnumber text,\n" +
-"	airtimeclass int,\n" +
-"	airtimeunits double,\n" +
-"	allocationcompletedate text,\n" +
-"	apn text,\n" +
-"	callednumber text,\n" +
-"	callingnumber text,\n" +
-"	carrierid int,\n" +
-"	cellid text,\n" +
-"	chargingid text,\n" +
-"	costcenterid int,\n" +
-"	downlinkvol bigint,\n" +
-"	duration double,\n" +
-"	exactusagedateonly date,\n" +
-"	exactusagetime text,\n" +
-"	fileid int,\n" +
-"	iccid text,\n" +
-"	imei text,\n" +
-"	imsi text,\n" +
-"	lineid bigint,\n" +
-"	linenumber int,\n" +
-"	mobilecountrycode text,\n" +
-"	mobilenetworkcode text,\n" +
-"	mobileoriginated boolean,\n" +
-"	msisdn text,\n" +
-"	network text,\n" +
-"	orgid int,\n" +
-"	orgurn text,\n" +
-"	plmn text,\n" +
-"	propertybag MAP<text, text>,\n" +
-"	recordtype text,\n" +
-"	roamingindicator text,\n" +
-"	roundingdate text,\n" +
-"	sender text,\n" +
-"	subscriptionid int,\n" +
-"	subscriptionurn text,\n" +
-"	surrecordtypeid int,\n" +
-"	tapcode text,\n" +
-"	uplinkvol bigint,\n" +
-"	usagetypeid int,\n" +
-"	PRIMARY KEY (partitionhash, hashcode)\n" +
-") WITH bloom_filter_fp_chance = 0.01\n" +
-"AND comment = ''\n" +
-"AND crc_check_chance = 1.0\n" +
-"AND dclocal_read_repair_chance = 0.1\n" +
-"AND default_time_to_live = 0\n" +
-"AND gc_grace_seconds = 864000\n" +
-"AND max_index_interval = 2048\n" +
-"AND memtable_flush_period_in_ms = 0\n" +
-"AND min_index_interval = 128\n" +
-"AND read_repair_chance = 0.0\n" +
-"AND speculative_retry = '99.0PERCENTILE'\n" +
-"AND caching = {\n" +
-"	'keys' : 'ALL',\n" +
-"	'rows_per_partition' : 'NONE'\n" +
-"}\n" +
-"AND compression = {\n" +
-"	'chunk_length_in_kb' : 64,\n" +
-"	'class' : 'LZ4Compressor',\n" +
-"	'enabled' : true\n" +
-"}\n" +
-"AND compaction = {\n" +
-"	'class' : 'SizeTieredCompactionStrategy',\n" +
-"	'max_threshold' : 32,\n" +
-"	'min_threshold' : 4\n" +
-"};")
+    "	partitionhash int,\n" +
+    "	hashcode text,\n" +
+    "	accountnumber text,\n" +
+    "	airtimeclass int,\n" +
+    "	airtimeunits double,\n" +
+    "	allocationcompletedate text,\n" +
+    "	apn text,\n" +
+    "	callednumber text,\n" +
+    "	callingnumber text,\n" +
+    "	carrierid int,\n" +
+    "	cellid text,\n" +
+    "	chargingid text,\n" +
+    "	costcenterid int,\n" +
+    "	downlinkvol bigint,\n" +
+    "	duration double,\n" +
+    "	exactusagedateonly date,\n" +
+    "	exactusagetime text,\n" +
+    "	fileid int,\n" +
+    "	iccid text,\n" +
+    "	imei text,\n" +
+    "	imsi text,\n" +
+    "	lineid bigint,\n" +
+    "	linenumber int,\n" +
+    "	mobilecountrycode text,\n" +
+    "	mobilenetworkcode text,\n" +
+    "	mobileoriginated boolean,\n" +
+    "	msisdn text,\n" +
+    "	network text,\n" +
+    "	orgid int,\n" +
+    "	orgurn text,\n" +
+    "	plmn text,\n" +
+    "	propertybag MAP<text, text>,\n" +
+    "	recordtype text,\n" +
+    "	roamingindicator text,\n" +
+    "	roundingdate text,\n" +
+    "	sender text,\n" +
+    "	subscriptionid int,\n" +
+    "	subscriptionurn text,\n" +
+    "	surrecordtypeid int,\n" +
+    "	tapcode text,\n" +
+    "	uplinkvol bigint,\n" +
+    "	usagetypeid int,\n" +
+    "	PRIMARY KEY (partitionhash, hashcode)\n" +
+    ") WITH bloom_filter_fp_chance = 0.01\n" +
+    "AND comment = ''\n" +
+    "AND crc_check_chance = 1.0\n" +
+    "AND dclocal_read_repair_chance = 0.1\n" +
+    "AND default_time_to_live = 0\n" +
+    "AND gc_grace_seconds = 864000\n" +
+    "AND max_index_interval = 2048\n" +
+    "AND memtable_flush_period_in_ms = 0\n" +
+    "AND min_index_interval = 128\n" +
+    "AND read_repair_chance = 0.0\n" +
+    "AND speculative_retry = '99.0PERCENTILE'\n" +
+    "AND caching = {\n" +
+    "	'keys' : 'ALL',\n" +
+    "	'rows_per_partition' : 'NONE'\n" +
+    "}\n" +
+    "AND compression = {\n" +
+    "	'chunk_length_in_kb' : 64,\n" +
+    "	'class' : 'LZ4Compressor',\n" +
+    "	'enabled' : true\n" +
+    "}\n" +
+    "AND compaction = {\n" +
+    "	'class' : 'SizeTieredCompactionStrategy',\n" +
+    "	'max_threshold' : 32,\n" +
+    "	'min_threshold' : 4\n" +
+    "};")
+query_descriptions = ["Buld Retrieval",
+    "Bulk Retreival: partitionhash = -1",
+    "Bulk Retreival: carrierid = 18000",
+    "Bulk Retreival: fileid = 278",
+    "Bulk Retreival: usagetypeid = 0"]
 
 # Global functions
 def do_logging(line):
@@ -106,6 +113,56 @@ def print_system_information():
     do_logging('\nCassandra Instance: ' + str(cass_instance) + ':' + str(cass_port))
     do_logging('\nClickhouse Table Definition:\n' + str(clkhs_table_definition))
     do_logging('\nCassandra Table Definition:\n' + str(cass_table_definition))
+
+def format_output(label, query_descriptors, num_rows_list, query_times_matrix, parse_times_matrix, rows_returned_matrix):
+    query =             ",Query Description,"
+    rows_selected =     label + ",Rows Selected,"
+    rows_returned =     ",Rows Returned,"
+    select_time =       ",Select Time,"
+    dataframe_time =    ",DataFrame Time,"
+    total =             ",Total,"
+
+    for i in range(0, len(query_descriptions)):
+        
+        # Loop vars
+        descriptor = query_descriptions[i]
+        select_time_list = query_times_matrix[i]
+        dataframe_time_list = parse_times_matrix[i]
+        rows_returned_list = rows_returned_matrix[i]
+
+        # Line 1
+        query = query + descriptor + ("," * (len(num_rows_list) + 1))
+
+        # Line 2
+        for num in num_rows_list:
+            rows_selected = rows_selected + str(num) + ","
+        rows_selected = rows_selected + ","
+
+        # Line 3
+        for num in rows_returned_list:
+            rows_returned = rows_returned + str(num) + ","
+        rows_returned = rows_returned + ","
+
+        # Line 4
+        for time in select_time_list:
+            select_time = select_time + ("%.2f" % (time)) + ","
+        select_time = select_time + ","
+
+        # Line 5
+        for time in dataframe_time_list:
+            dataframe_time = dataframe_time + ("%.2f" % (time)) + ","
+        dataframe_time = dataframe_time + ","
+
+        # Line 6
+        for time1, time2 in zip(select_time_list, dataframe_time_list):
+            if (time1 + time2 < 0):
+                total = total + "-1,"
+            else:
+                total = total + ("%.2f" % (time1 + time2)) + ","
+        total = total + ","
+
+    final = query + "\n" + rows_selected + "\n" + rows_returned + "\n" + select_time + "\n" + dataframe_time + "\n" + total + "\n"
+    return final
 
 # Global Execution
 print_system_information()
@@ -163,6 +220,7 @@ clkhs_select_query_prefix = ("SELECT visitParamExtractInt(Message, 'partitionhas
     "visitParamExtractInt(Message, 'usagetypeid') AS usagetypeid \n" +
     "FROM radius.udr \n")
 clkhs_select_query_midfixes = ["LIMIT ",
+    "WHERE partitionhash = -1 \n" + "LIMIT ",
     "WHERE carrierid = 18000 \n" + "LIMIT ",
     "WHERE fileid = 278 \n" + "LIMIT ",
     "WHERE usagetypeid = 0 \n" + "LIMIT "]
@@ -170,14 +228,15 @@ clkhs_UDR_df = pd.DataFrame()
 
 # Functions
 def clkhs_start_artificial_load():
-    print('placeholder')
+    i = 0
 
 # clkhs setup
 clk_settings = {'max_threads': 8, 'max_block_size': 5000}
 client = Client(host=clkhs_instance, port='', settings=clk_settings, connect_timeout=60, send_receive_timeout=900, sync_request_timeout=120)
 
-clkhs_query_timing = []
-clkhs_parse_timing = []
+clkhs_query_timing_matrix = []
+clkhs_parse_timing_matrix = []
+clkhs_rows_returned_matrix = []
 
 # Time the retrieval of various number of records
 # Iterate over the queries
@@ -185,11 +244,13 @@ for clkhs_select_query_midfix in clkhs_select_query_midfixes:
     do_logging("\nCurrent Query: " + clkhs_select_query_prefix + clkhs_select_query_midfix)
     clkhs_avg_query_times = []
     clkhs_avg_parse_times = []
+    clkhs_rows_returned_list = []
 
     # Iterate over different limits
     for num_rows in num_rows_list:
         total_query_time = 0.0
         total_parse_time = 0.0
+        rows_returned = 0
         do_break = False
 
         # Iterate set number of times, for repetitions sake
@@ -197,6 +258,9 @@ for clkhs_select_query_midfix in clkhs_select_query_midfixes:
             query_time = 0.0
             parse_time = 0.0
             settings = {'max_block_size': 5000}
+
+            # Wait
+            time.sleep(random.randint(50, 61))
 
             # Do the query and parsing
             clkhs_start_artificial_load()
@@ -216,6 +280,9 @@ for clkhs_select_query_midfix in clkhs_select_query_midfixes:
             # Aggregate
             total_query_time = total_query_time + query_time
             total_parse_time = total_parse_time + parse_time
+            rows_returned = int(clkhs_UDR_df.size / num_cols)
+
+            do_logging('Iteration %d: %.2f' % (i, query_time + parse_time))
 
             # Break condition
             if (num_rows > int(clkhs_UDR_df.size / num_cols)):
@@ -226,6 +293,7 @@ for clkhs_select_query_midfix in clkhs_select_query_midfixes:
         avg_parse_time = total_parse_time / num_repetitions
         clkhs_avg_query_times.append(avg_query_time)
         clkhs_avg_parse_times.append(avg_parse_time)
+        clkhs_rows_returned_list.append(rows_returned)
 
         # Logging
         do_logging("\nResults for %d records averaged over %d repetitions:" % (num_rows, num_repetitions))
@@ -235,14 +303,18 @@ for clkhs_select_query_midfix in clkhs_select_query_midfixes:
         do_logging("Total time: %.2f seconds" % (avg_parse_time + avg_query_time))
 
         if (do_break):
+            for j in range(0, len(num_rows_list) - len(clkhs_avg_query_times)):
+                clkhs_avg_query_times.append(-1.0)
+                clkhs_avg_parse_times.append(-1.0)
+                clkhs_rows_returned_list.append(-1)
             break
     
     # Add to lists
-    clkhs_query_timing.append(clkhs_avg_query_times)
-    clkhs_parse_timing.append(clkhs_avg_parse_times)
+    clkhs_query_timing_matrix.append(clkhs_avg_query_times)
+    clkhs_parse_timing_matrix.append(clkhs_avg_parse_times)
+    clkhs_rows_returned_matrix.append(clkhs_rows_returned_list)
         
-do_logging(clkhs_query_timing)
-do_logging(clkhs_parse_timing)
+csv_file.write(format_output("CH", query_descriptions, num_rows_list, clkhs_query_timing_matrix, clkhs_parse_timing_matrix, clkhs_rows_returned_matrix))
 
 
 
@@ -255,7 +327,8 @@ from cassandra.auth import PlainTextAuthProvider
 
 # Variables/constants
 cass_select_query_prefixes = ['SELECT * FROM "udr" LIMIT ', 
-    'SELECT * FROM "udr" WHERE carrierid = 18000 LIMIT ', 
+    'SELECT * FROM "udr" WHERE partitionhash = -1',
+    'SELECT * FROM "udr" WHERE carrierid = 18000 LIMIT '
     'SELECT * FROM "udr" WHERE fileid = 278 LIMIT ', 
     'SELECT * FROM "udr" WHERE usagetypeid = 0 LIMIT ']
 cass_UDR_df = pd.DataFrame()
@@ -266,14 +339,15 @@ def pandas_factory(colnames, rows):
     return pd.DataFrame(rows, columns=colnames)
 
 def cass_start_artificial_load():
-    print('placeholder')
+    pi = 0
 
 # cassandra setup
 authentication = PlainTextAuthProvider(username='devadmin', password='Keys2TheK1ngd0m')
 cluster = Cluster([cass_instance], port=cass_port, auth_provider=authentication)
 
-cass_query_timing = []
-cass_parse_timing = []
+cass_query_timing_matrix = []
+cass_parse_timing_matrix = []
+cass_rows_returned_matrix = []
 
 # Time the retrieval of various number of records for various queries
 # Iterate over the queries
@@ -283,6 +357,7 @@ for cass_select_query_prefix in cass_select_query_prefixes:
     do_logging("\nCurrent Query: " + cass_select_query_prefix)
     cass_avg_query_times = []
     cass_avg_parse_times = []
+    cass_rows_returned_list = []
 
     try:
 
@@ -290,6 +365,7 @@ for cass_select_query_prefix in cass_select_query_prefixes:
         for num_rows in num_rows_list:
             total_query_time = 0.0
             total_parse_time = 0.0
+            rows_returned = 0
             do_break = False
             session = cluster.connect('CassandraPractice')
             session.row_factory = pandas_factory
@@ -300,10 +376,13 @@ for cass_select_query_prefix in cass_select_query_prefixes:
                 query_time = 0.0
                 parse_time = 0.0
 
+                # Wait
+                time.sleep(random.randint(50, 61))
+
                 # Do the initial query and parsing
                 cass_start_artificial_load()
                 start_time = time.perf_counter()
-                rows = session.execute(cass_select_query_prefix + str(num_rows) + ';', timeout=None)
+                rows = session.execute(cass_select_query_prefix + str(num_rows) + ' ALLOW FILTERING;', timeout=None)
                 mid_time = time.perf_counter()
                 cass_UDR_list = []
                 cass_UDR_list.extend(rows._current_rows.values.tolist())
@@ -333,6 +412,9 @@ for cass_select_query_prefix in cass_select_query_prefixes:
                 # Aggregate
                 total_query_time = total_query_time + query_time
                 total_parse_time = total_parse_time + parse_time
+                rows_returned = int(cass_UDR_df.size / num_cols)
+
+                do_logging('Iteration %d: %.2f' % (i, query_time + parse_time))
 
                 if (num_rows > int(cass_UDR_df.size / num_cols)):
                     do_break = True
@@ -342,10 +424,11 @@ for cass_select_query_prefix in cass_select_query_prefixes:
             avg_parse_time = total_parse_time / num_repetitions
             cass_avg_query_times.append(avg_query_time)
             cass_avg_parse_times.append(avg_parse_time)
+            cass_rows_returned_list.append(rows_returned)
 
             # Logging
             do_logging("\nResults for %d records averaged over %d repetitions:" % (num_rows, num_repetitions))
-            do_logging("Actual rows fetched: %d" % (int(clkhs_UDR_df.size / num_cols)))
+            do_logging("Actual rows fetched: %d" % (int(cass_UDR_df.size / num_cols)))
             do_logging("Time to execute select: %.2f seconds" % (avg_query_time))
             do_logging("Time to put into dataframe: %.2f seconds" % (avg_parse_time))
             do_logging("Total time: %.2f seconds" % (avg_parse_time + avg_query_time))
@@ -353,28 +436,34 @@ for cass_select_query_prefix in cass_select_query_prefixes:
             # Cleanup
             session.shutdown()
             if (do_break):
+                for j in range(0, len(num_rows_list) - len(cass_avg_query_times)):
+                    cass_avg_query_times.append(-1.0)
+                    cass_avg_parse_times.append(-1.0)
+                    cass_rows_returned_list.append(-1)
                 break
 
         # Add to lists
-        cass_query_timing.append(cass_avg_query_times)
-        cass_parse_timing.append(cass_avg_parse_times)
+        cass_query_timing_matrix.append(cass_avg_query_times)
+        cass_parse_timing_matrix.append(cass_avg_parse_times)
+        cass_rows_returned_matrix.append(cass_rows_returned_list)
             
     except cassandra.ReadTimeout:
         do_logging('Coordinator Node Timeout')
-        cass_query_timing.append([0.0] * num_repetitions)
-        cass_parse_timing.append([0.0] * num_repetitions)
+        cass_query_timing_matrix.append([-1.0] * num_repetitions)
+        cass_parse_timing_matrix.append([-1.0] * num_repetitions)
+        cass_rows_returned_matrix.append([-1] * num_repetitions)
     except cassandra.Timeout:
         do_logging('Timeout')
-        cass_query_timing.append([0.0] * num_repetitions)
-        cass_parse_timing.append([0.0] * num_repetitions)
+        cass_query_timing_matrix.append([-1.0] * num_repetitions)
+        cass_parse_timing_matrix.append([-1.0] * num_repetitions)
+        cass_rows_returned_matrix.append([-1] * num_repetitions)
     except cassandra.ReadFailure:
         do_logging('Invalid Query: At least one replica failed')
-        cass_query_timing.append([0.0] * num_repetitions)
-        cass_parse_timing.append([0.0] * num_repetitions)
+        cass_query_timing_matrix.append([-1.0] * num_repetitions)
+        cass_parse_timing_matrix.append([-1.0] * num_repetitions)
+        cass_rows_returned_matrix.append([-1] * num_repetitions)
 
-
-do_logging(cass_query_timing)
-do_logging(cass_parse_timing)
+format_output("C*", query_descriptions, num_rows_list, cass_query_timing_matrix, cass_parse_timing_matrix, cass_rows_returned_matrix)
 
 # Cleanup
 file.close()
--- filler.py
+++ filler.py
@@ -9,86 +9,86 @@ from pandas.io.json import json_normalize
 
 
 
-# Clickhouse specific code
-print('Beginning Clickhouse Test')
+# # Clickhouse specific code
+# print('Beginning Clickhouse Test')
 
-# Setup
-from clickhouse_driver import Client
-clk_settings = {'max_threads': 8, 'max_block_size': 5000}
-client = Client(host='192.168.5.60', port='', settings=clk_settings, connect_timeout=60, send_receive_timeout=900, sync_request_timeout=120)
-create_table_query = ("CREATE TABLE radius.udr ( " +
-    "CreateDate DateTime default now(), " +
-    "Message String " +
-    ") ENGINE = MergeTree() PARTITION BY toYYYYMM(CreateDate) order by tuple()")
-insert_query = "INSERT INTO radius.udr(Message) Format VALUES "
+# # Setup
+# from clickhouse_driver import Client
+# clk_settings = {'max_threads': 8, 'max_block_size': 5000}
+# client = Client(host='192.168.5.60', port='', settings=clk_settings, connect_timeout=60, send_receive_timeout=900, sync_request_timeout=120)
+# create_table_query = ("CREATE TABLE radius.udr ( " +
+#     "CreateDate DateTime default now(), " +
+#     "Message String " +
+#     ") ENGINE = MergeTree() PARTITION BY toYYYYMM(CreateDate) order by tuple()")
+# insert_query = "INSERT INTO radius.udr(Message) Format VALUES "
 
-# Create table
-client.execute(create_table_query)
+# # Create table
+# client.execute(create_table_query)
 
-def insert_rows(clkhs_list):
-    print('Adding messages')
-    UDR_clkhs_df = pd.DataFrame(clkhs_list, columns = ['Message'])
-    client.execute(insert_query, UDR_clkhs_df.to_dict('r'))
+# def insert_rows(clkhs_list):
+#     print('Adding messages')
+#     UDR_clkhs_df = pd.DataFrame(clkhs_list, columns = ['Message'])
+#     client.execute(insert_query, UDR_clkhs_df.to_dict('r'))
 
-UDR_data_df = pd.read_csv('backup.csv')
-UDR_clkhs_list = []
-index = 0
-for row in UDR_data_df.itertuples(name=None):
-    rowdict = {
-        "partitionhash":int(row[1]),
-        "hashcode":str(row[2]),
-        "accountnumber":str(row[3]),
-        "airtimeclass":int(row[4]),
-        "airtimeunits":float(row[5]),
-        "allocationcompletedate":str(row[6]),
-        "apn":str(row[7]),
-        "callednumber":str(row[8]),
-        "callingnumber":str(row[9]),
-        "carrierid":int(row[10]),
-        "cellid":str(row[11]),
-        "chargingid":str(row[12]),
-        "costcenterid":int(row[13]),
-        "downlinkvol":int(row[14]),
-        "duration":float(row[15]),
-        "exactusagedateonly":str(row[16]),
-        "exactusagetime":str(row[17]),
-        "fileid":int(row[18]),
-        "iccid":str(row[19]),
-        "imei":str(row[20]),
-        "imsi":str(row[21]),
-        "lineid":int(row[22]),
-        "linenumber":int(row[23]),
-        "mobilecountrycode":str(row[24]),
-        "mobilenetworkcode":str(row[25]),
-        "mobileoriginated":bool(row[26]),
-        "msisdn":str(row[27]),
-        "network":str(row[28]),
-        "orgid":int(row[29]),
-        "orgurn":str(row[30]),
-        "plmn":str(row[31]),
-        "propertybag":str(row[32]),
-        "recordtype":str(row[33]),
-        "roamingindicator":str(row[34]),
-        "roundingdate":str(row[35]),
-        "sender":str(row[36]),
-        "subscriptionid":int(row[37]),
-        "subscriptionurn":str(row[38]),
-        "surrecordtypeid":int(row[39]),
-        "tapcode":str(row[40]),
-        "uplinkvol":int(row[41]),
-        "usagetypeid":int(row[42])
-    }
-    row_json = json.dumps(rowdict).replace(' ', '').replace('":NaN,"', '":null,"').replace('":"nan","', '":null,"')
-    UDR_clkhs_list.append(row_json)
-    index = index + 1
+# UDR_data_df = pd.read_csv('backup.csv')
+# UDR_clkhs_list = []
+# index = 0
+# for row in UDR_data_df.itertuples(name=None):
+#     rowdict = {
+#         "partitionhash":int(row[1]),
+#         "hashcode":str(row[2]),
+#         "accountnumber":str(row[3]),
+#         "airtimeclass":int(row[4]),
+#         "airtimeunits":float(row[5]),
+#         "allocationcompletedate":str(row[6]),
+#         "apn":str(row[7]),
+#         "callednumber":str(row[8]),
+#         "callingnumber":str(row[9]),
+#         "carrierid":int(row[10]),
+#         "cellid":str(row[11]),
+#         "chargingid":str(row[12]),
+#         "costcenterid":int(row[13]),
+#         "downlinkvol":int(row[14]),
+#         "duration":float(row[15]),
+#         "exactusagedateonly":str(row[16]),
+#         "exactusagetime":str(row[17]),
+#         "fileid":int(row[18]),
+#         "iccid":str(row[19]),
+#         "imei":str(row[20]),
+#         "imsi":str(row[21]),
+#         "lineid":int(row[22]),
+#         "linenumber":int(row[23]),
+#         "mobilecountrycode":str(row[24]),
+#         "mobilenetworkcode":str(row[25]),
+#         "mobileoriginated":bool(row[26]),
+#         "msisdn":str(row[27]),
+#         "network":str(row[28]),
+#         "orgid":int(row[29]),
+#         "orgurn":str(row[30]),
+#         "plmn":str(row[31]),
+#         "propertybag":str(row[32]),
+#         "recordtype":str(row[33]),
+#         "roamingindicator":str(row[34]),
+#         "roundingdate":str(row[35]),
+#         "sender":str(row[36]),
+#         "subscriptionid":int(row[37]),
+#         "subscriptionurn":str(row[38]),
+#         "surrecordtypeid":int(row[39]),
+#         "tapcode":str(row[40]),
+#         "uplinkvol":int(row[41]),
+#         "usagetypeid":int(row[42])
+#     }
+#     row_json = json.dumps(rowdict).replace(' ', '').replace('":NaN,"', '":null,"').replace('":"nan","', '":null,"')
+#     UDR_clkhs_list.append(row_json)
+#     index = index + 1
 
-    if (index % 100000 == 0):
-        print(row_json)
-        index = 0
-        insert_rows(UDR_clkhs_list)
-        UDR_clkhs_list = []
+#     if (index % 100000 == 0):
+#         print(row_json)
+#         index = 0
+#         insert_rows(UDR_clkhs_list)
+#         UDR_clkhs_list = []
 
-insert_rows(UDR_clkhs_list)
+# insert_rows(UDR_clkhs_list)
 
 
 
@@ -108,6 +108,7 @@ session.row_factory = pandas_factory
 session.default_fetch_size = None
 
 # queries
+# NOTE: This driver does not support COPY. USE cqlsh -u devadmin -p Keys2TheK1ngd0m 172.30.100.206
 cass_select_query = 'COPY "udr_loadtest" TO \'backup.csv\''
 cass_insert_query = 'COPY "udr" FROM \'backup.csv\''
 
